917030af541a7c6d02e896a1ba0ed5c70a6d1e3a
Andrei Korikov <kpanic666@gmail.com>
2021-11-18 17:45:25 +0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Andrei Korikov on 18.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
